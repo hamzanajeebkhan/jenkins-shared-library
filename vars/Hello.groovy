528e663c7791e2505658fb63c1e3cbd4e9ce7754
Hamza Najeeb Khan <hmza26@gmail.com>
2024-09-15 15:39:06 +0500

    Major change

--- vars/Hello.groovy
+++ vars/Hello.groovy
@@ -2,15 +2,9 @@
 def call() {
 pipeline {
     
-    agent { label "testing-agent" }
+    agent { label "app-1" }
 
     stages {
-        stage('Hello') {
-            steps {
-                echo 'Hello World'
-            }
-        }
-
         stage('New Stage') {
             steps {
                 echo 'Hello Stage 2'
@@ -19,9 +13,9 @@ pipeline {
             }
         }
 
-        stage('Shared Library work') {
+        stage('Shared Library work from branch') {
             steps {
-                echo 'This is coming from Shared Library'
+                echo 'This is coming from Shared Library from branch'
             }
         }
     }
