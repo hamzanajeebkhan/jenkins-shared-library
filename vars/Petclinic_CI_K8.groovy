839667f3592eae621c2693764be84d95840d6794
Hamza Najeeb Khan <hmza26@gmail.com>
2024-09-22 00:28:11 +0500

    Minor change

--- vars/Petclinic_CI_K8.groovy
+++ vars/Petclinic_CI_K8.groovy
@@ -40,21 +40,6 @@ def call () {
                 }
             }
         }
-
-        stage('Build and Push Docker Image') {
-            steps {
-                container('kaniko') {
-                    withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', usernameVariable: 'DOCKER_HUB_USR', passwordVariable: 'DOCKER_HUB_PSW')]) {
-                    script {
-                            sh """
-                                echo '{"auths":{"https://index.docker.io/v1/":{"auth":"'"\$(echo -n ${DOCKER_HUB_USR}:${DOCKER_HUB_PSW} | base64)"'"}}}' > /kaniko/.docker/config.json
-                                /kaniko/executor --dockerfile="/Dockerfile" --context `pwd` --destination ${DOCKER_HUB_REPO}:${IMAGE_TAG}
-                            """
-                        }
-                    }
-                }
-            }
-        }
     }
 }
 }
